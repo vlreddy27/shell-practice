4886c3c33978d02e09818bafe4783317a9aa4006
vlreddy27 <laxmareddy21@gmail.com>
2025-07-17 17:30:09 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: hey $PERSON2, How are you"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing"
+echo "$PERSON1:: I am fine too. WHat's up?"
+echo "$PERSON2:: Nothing, just planning to go to Mars now, Will you come" 
+echo "$PERSON1:: Sorry you carry on! I will come once you come back"
