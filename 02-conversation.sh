566cea442a3e3336bb7ffbfd6b1c5e01577c7a64
vlreddy27 <laxmareddy21@gmail.com>
2025-07-17 17:12:44 +0530

    conversation started

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,6 +2,6 @@
 
 echo "trump: hey musk, How are you"
 echo "Musk: Hello Trump, I am fine. How are you doing"
-ehco "Trump: I am fine too. WHat's up?"
+echo "Trump: I am fine too. WHat's up?"
 echo "Musk: Nothing, just planning to go to Mars now, Will you come" 
 echo "Trump: Sorry you carry on! I will come once you come back"
