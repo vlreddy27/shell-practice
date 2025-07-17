fc751e7bf500cc51873e86fb7eac48060b10a7a3
vlreddy27 <laxmareddy21@gmail.com>
2025-07-17 17:19:58 +0530

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=$PERSON1
-PERSON2=$PERSON2
+PERSON1=Trump
+PERSON2=Musk
 
 echo "$PERSON1: hey $PERSON2, How are you"
 echo "$PERSON2: Hello $PERSON1, I am fine. How are you doing"
